bc4198b0d070d1c9e5f3a68cc15c35d71daa0c4e
Muhammed Zain <muhammedzain2256@gmail.com>
2023-10-14 15:08:09 +0500

    Navbar Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
-import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import './globals.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
 
 export const metadata: Metadata = {
   title: 'Travel',
@@ -17,7 +17,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Navbar/>
+          <main className="relative overflow-hidden">
+            {children}
+          </main>
+        <Footer/>
       </body>
     </html>
   )
